b168eb25cb723a301691145d51900e3912fb5638
Luiza Carvalho <luizacarvalho.arq@gmail.com>
2023-01-27 11:30:20 -0300

    Create and implement button component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 //import Button from './components/button/button'
 import Paragraph from './components/paragraph/paragraph'
+import Buttons from './components/buttons/buttons'
 
 import './App.css'
+
 
 const changeTextColor = () => {
   const p = document.querySelector('.paragraph')
@@ -13,7 +15,7 @@ function App() {
   return (
     <>
       <Paragraph changeTextColor={changeTextColor}/>
-     
+      <Buttons/>
     </>
   )
 }
